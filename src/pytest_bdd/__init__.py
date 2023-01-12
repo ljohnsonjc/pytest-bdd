2431c6804b0e988ae006520975ce174a7f454408
Leight Johnson <leight.johnson@jumpcloud.com>
2023-01-11 17:10:39 -0700

    add version to init

--- src/pytest_bdd/__init__.py
+++ src/pytest_bdd/__init__.py
@@ -4,4 +4,7 @@ from __future__ import annotations
 from pytest_bdd.scenario import scenario, scenarios
 from pytest_bdd.steps import given, step, then, when
 
+# Added for ddtrace
+__version__ = "6.1.1"
+
 __all__ = ["given", "when", "step", "then", "scenario", "scenarios"]
